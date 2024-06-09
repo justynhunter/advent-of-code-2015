55c007839ba89d5cbefc92a6d3babdc36caa3b6a
Justyn Hunter <justynhunter@gmail.com>
2024-06-08 22:59:07 -0500

    day1 done

---
+++ bin/main.ml
@@ -0,0 +1,7 @@
+open Aoc2015
+
+let () = 
+    let d01p1 = Day01.solve_part1 in
+    print_endline ("Day 1, Part 1: " ^ (string_of_int d01p1));;
+    let d01p2 = Day01.solve_part2 in
+    print_endline ("Day 1, Part 2: " ^ (string_of_int d01p2));;
